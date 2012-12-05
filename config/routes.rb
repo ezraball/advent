bda3f931c618a88fe7f646fd5cd5607c7662de0c
Ezra Ball <administrator@Ezra-Balls-MacBook-Pro-5.local>
2012-12-05 01:39:41 -0500

    better admin schtuff

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ Advent::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'calendars#show', :id => 1
 
   # See how all your routes lay out with "rake routes"
 
